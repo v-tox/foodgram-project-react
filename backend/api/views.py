e532d340304f9b727181ed4c98cf1152c3148b87
Ника Истомина <v-ist@dzen.ru>
2023-06-29 18:31:24 +0500

    beznadyoga

--- backend/api/views.py
+++ backend/api/views.py
@@ -15,10 +15,11 @@ from users.models import Follow, User
 from .filters import IngredientFilter, RecipeFilter
 from .serializers import (UserSerializer,
                           IngredientSerializer,
-                          NewUserSerializer,
                           TagSerializer,
                           RecipeSerializer,
-                          FollowSerializer,)
+                          FollowSerializer,
+                          # NewUserSerializer
+                          )
 from .permissions import (IsAuthorOrAdminOrReadOnly)
 
 
@@ -78,25 +79,25 @@ class UserViewSet(viewsets.ModelViewSet):
             serializer = self.get_serializer(user)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    @action(
-        methods=('post',),
-        detail=False,
-        permission_classes=(AllowAny,)
-    )
-    def register(self, request):
-        serializer = NewUserSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(
-                serializer.data,
-                status=status.HTTP_201_CREATED
-            )
-        else:
-            return Response(
-                serializer.errors,
-                status=status.HTTP_400_BAD_REQUEST
-            )
+#    @action(
+#        methods=('post',),
+#        detail=False,
+#        permission_classes=(AllowAny,)
+#    )
+#    def register(self, request):
+#        serializer = NewUserSerializer(data=request.data)
+#
+#        if serializer.is_valid():
+#            serializer.save()
+#            return Response(
+#                serializer.data,
+#                status=status.HTTP_201_CREATED
+#            )
+#        else:
+#            return Response(
+#                serializer.errors,
+#                status=status.HTTP_400_BAD_REQUEST
+#            )
 
 
 class TagViewSet(viewsets.ReadOnlyModelViewSet):
