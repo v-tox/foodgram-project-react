0c0aede9b8e66d9d1aaee0617241b5b71e184d65
Ника Истомина <v-ist@dzen.ru>
2023-06-30 15:47:03 +0500

    db4

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -67,7 +67,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB', 'postgres'),
         'USER': os.getenv('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', ''),
+        'HOST': os.getenv('DB_HOST', 'db'),
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
